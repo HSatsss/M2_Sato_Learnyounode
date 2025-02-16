d722124c6c6a9c4ec819452e03d9e6bfb9796064
HSatsss <hannasato14@gmail.com>
2025-02-16 21:17:15 +0800

    COMMIT

--- mymodule.js
+++ mymodule.js
@@ -5,7 +5,7 @@ module.exports = function (dir, ext, callback) {
   fs.readdir(dir, (err, files) => {
     if (err) return callback(err);
 
-    const filteredFiles = files.filter(file => path.extname(file) === '.' + ext);
-    callback(null, filteredFiles);
+    const filtered = files.filter(file => path.extname(file) === '.' + ext);
+    callback(null, filtered);
   });
-};S
+};
